3cc243af37cab166163fe1149e1cfe6057ffa2ab
hdk <2890241339@qq.com>
2021-02-10 15:41:03 +0800

    new Hello.java

---
+++ github01/src/Hello.java
@@ -0,0 +1,16 @@
+/*
+@Author:南柯一梦
+@Contact:2890241339@qq.com
+@Date:2021/2/10
+*/
+
+//import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+
+
+}
