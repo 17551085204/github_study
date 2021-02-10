63dce76797b1e209b405c2374a853e7fb765e17c
hdk <2890241339@qq.com>
2021-02-10 15:55:56 +0800

    11

--- github01/src/Hello.java
+++ github01/src/Hello.java
@@ -10,6 +10,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("123456");
+        System.out.println("sasada");
 
     }
 }
